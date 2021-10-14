666734419db95711fe6ed6a73de661a47c279e13
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-14 10:05:56 -0400

    commit tree e970db9f7fd50f1bfbc299e8323b0a1ba2d9cfaf

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+
+
+
+
+function superbowlWin(element) {
+    const newArray = element.find(element => element.result === "W");
+        if (newArray) {
+            return newArray.year
+        } else {
+            return undefined
+        }
+}
+
 
